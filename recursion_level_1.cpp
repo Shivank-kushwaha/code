9db5827fceeff506b0abcbb09aeb093c54f2d10c
shivank kushwaha <shivankkushwaha793@gmail.com>
2026-07-10 17:04:24 +0530

    first level done of recursion

--- recursion_level_1.cpp
+++ recursion_level_1.cpp
@@ -4,91 +4,97 @@ using namespace std;
 //Question No.1
 void print(int n){
     if(n == 0){
-        return;
+        return ;
     }
+
     print(n-1);
     cout<<n<<" ";
 }
 
-//Question No.2
-void printNT01(int n){
+//Question NO.2
+void printNto1(int n){
     if(n == 0){
-        return;
+        return ;
     }
     cout<<n<<" ";
-    printNT01(n-1);
+    printNto1(n-1);
 }
 
 //Question No.3
-void printEvenNumber(int n){
+void printEvenNUmbers(int n){
     if(n == 0){
-        return;
+        return ;
     }
-    printEvenNumber(n-1);
+
+    printEvenNUmbers(n-1);
     if(n % 2 == 0){
         cout<<n<<" ";
     }
 }
 
-//Question No. 4
-void printOddNumber(int n){
+//Question No.4
+void printOddNUmbers(int n){
     if(n == 0){
-        return;
+        return ;
     }
-    printOddNumber(n-1);
+
+    printOddNUmbers(n-1);
     if(n % 2 != 0){
         cout<<n<<" ";
     }
 }
 
-//Question No.5
-int sum(int n){
+//Question NO.5
+int printSum(int n){
     if(n == 0){
         return 0;
     }
-    return sum(n-1) + n;
+
+    return n + printSum(n-1);
 }
 
-//Question No.6
-int factorial(int n){
-    if(n == 0){
+//Question NO.6
+int printFactorial(int n){
+    if(n == 1){
         return 1;
     }
 
-    return factorial(n-1) * n;
+    return n * printFactorial(n-1);
 }
 
 //Question No.7
 int power(int x, int n){
-    if(n == 0){
-        return 1;
+    if(n == 1){
+        return x;
     }
 
     return x * power(x,n-1);
 }
 
-//Question No.7
 int fibonachhi(int n){
-    if(n == 0 || n == 1){
+    if(n == 1 || n == 0){
         return n;
     }
+
     return fibonachhi(n-1) + fibonachhi(n-2);
 }
 
-void printFibonachhi(int n){
-    for(int i=n; i>=0; i--){
-        cout<<fibonachhi(i)<<" ";
-    };
-}
-
-int sumOfDigit(int n){
+int sumOfDigits(int n){
     if(n == 0){
-        return 0;
+        return n;
     }
-    return n % 10 + sumOfDigit(n / 10);
+
+    int lastDigit = n % 10;
+    return lastDigit + sumOfDigits(n=n/10);
 }
 
+int printFibonachhi(int n){
+    for(int i=0; i<=n; i++){
+        cout<<fibonachhi(i)<<" ";
+    }
+    cout<<endl;
+}
 int main(){
-    cout<<sumOfDigit(153)<<endl;
+    cout<<sumOfDigits(531)<<endl;
     return 0;
 }
