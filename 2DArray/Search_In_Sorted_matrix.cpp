e6acb95f7f9a3b59811a7e1bb718148c77d09f09
shivank kushwaha <shivankkushwaha793@gmail.com>
2026-07-24 19:00:26 +0530

    Binary search in 2D Array

--- 2DArray/Search_In_Sorted_matrix.cpp
+++ 2DArray/Search_In_Sorted_matrix.cpp
@@ -16,6 +16,32 @@ int search(int arr[][4], int n, int m,int key){
     return -1;
 }
 
+int binarySearch(int arr[][4], int n, int m, int key){
+
+    for(int i=0; i<n; i++){
+
+        int start = 0;
+        int end = m-1;
+
+        while(start <= end){
+            int mid = (start + end) / 2;
+
+            if(arr[i][mid] == key){
+                cout<<"["<<i<<","<<mid<<"]"<<endl;
+                return i;
+            }
+
+            if(key < arr[i][mid]){
+                end = mid - 1;
+            }else{
+                start = mid + 1;
+            }
+        }
+    }
+    
+    return -1;
+}
+
 int main(){
     int arr[4][4] = {
         {10,20,30,40},
@@ -24,7 +50,7 @@ int main(){
         {32,33,39,50}
     };
 
-    int result = search(arr,4,4,33);
+    int result = binarySearch(arr,4,4,78);
     if(result == -1){
         cout<<"key Not found"<<endl;
     }
