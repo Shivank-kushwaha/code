46fcb0274cb23532c50d2cd29e76503fc84eca8c
shivank kushwaha <shivankkushwaha793@gmail.com>
2026-07-25 18:46:58 +0530

    input taking

--- char Arrays & Strings/char.cpp
+++ char Arrays & Strings/char.cpp
@@ -1,9 +1,15 @@
 #include<iostream>
+#include<cstring>
 using namespace std;
 
 int main(){
-    char ch = 'f';
-    int pos = ch - 'a';
-    cout<<pos<<endl;
+    char word[30];
+    cin.getline(word,30);
+
+    int n = sizeof(word) / sizeof(char);
+
+    cout<<"your word was: "<<word<<endl;
+    cout<<"size of word: "<<n<<endl;
+    cout<<"Length of word: "<<strlen(word)<<endl;
     return 0;
 }
