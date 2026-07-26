1f06f30a4a5a50ff469587ab35ab0f6027185fda
shivank kushwaha <shivankkushwaha793@gmail.com>
2026-07-26 14:51:06 +0530

    string convert to lower case

---
+++ char Arrays & Strings/toLowerCase.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+void toLowerCase(char word[], int n){
+
+    for(int i=0; i<n; i++){
+        char ch = word[i];
+
+        if(ch >= 'a' && ch <= 'z'){
+            continue;
+        }else {
+            word[i] = ch -'A' + 'a';
+        }
+    }
+}
+
+int main(){
+    char word[] = "globalINDIA";
+
+    toLowerCase(word, strlen(word));
+
+    cout<<word<<endl;
+    return 0;
+}
