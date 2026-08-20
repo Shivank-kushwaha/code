ffa9427ef93335e02938981e0b49160599125ffa
shivank kushwaha <shivankkushwaha793@gmail.com>
2026-08-20 22:13:01 +0530

    Different varisons of climbing stairs

---
+++ DP/climbingStairsVariationDP.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+//Tabulation DP
+int countWays(int n){
+    vector<int>dp(n+1, 0);
+
+    dp[0] = 1;
+    dp[1] = 1;
+    dp[2] = 2;
+
+    for(int i=3; i<=n; i++){
+        dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+    }
+
+    return dp[n];
+}
+
+int main(){
+    int n = 4;
+    cout<<"Total Ways: "<<countWays(n)<<endl;
+    return 0;
+}
