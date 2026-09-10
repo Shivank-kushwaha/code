2cc8af043fed91d01c8bf82477eac42877110484
shivank kushwaha <shivankkushwaha793@gmail.com>
2026-09-10 14:48:59 +0530

    find maximum sum using sliding window

--- slidingWindow/maxSum.cpp
+++ slidingWindow/maxSum.cpp
@@ -3,20 +3,20 @@
 using namespace std;
 
 int maxSum(vector<int> arr, int w){
-    int currSum = 0;
-
     int n = arr.size();
+
+    int maximum = 0;
+
     for(int i=0; i<w; i++){
-        currSum += arr[i];
+        maximum += arr[i];
     }
 
-    int maximum = currSum;
+    int currSum = maximum;
 
-   
     for(int i=1; i<=n-w; i++){
 
-        currSum = (currSum - arr[i-1]) + arr[i+w-1];
-       
+        currSum = currSum - arr[i-1] + arr[i+w-1];   
+        
         if(currSum > maximum){
             maximum = currSum;
         }
@@ -26,8 +26,8 @@ int maxSum(vector<int> arr, int w){
 }
 
 int main(){
-    vector<int>arr = {3,8,2,5,7,6,12};
+    vector<int> arr = {3,8,2,5,7,6,12};
     int w = 3;
-    cout<<"maximum sum is: "<<maxSum(arr,w)<<endl;
+    cout<<"maximum is: "<<maxSum(arr,w)<<endl;
     return 0;
 }
