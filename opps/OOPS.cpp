1136d414ebeec631aa2873c93ad3b7b91c1a49d2
shivank kushwaha <shivankkushwaha793@gmail.com>
2026-09-17 16:46:13 +0530

    learing basics of oops

--- opps/OOPS.cpp
+++ opps/OOPS.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-class student{
+class Student{
     public:
     string name;
     float cgpa;
@@ -11,10 +11,33 @@ class student{
     }
 };
 
+class User{
+    public: 
+    string username;
+    string password;
+    string bio;
+
+    void deactivate(){
+        cout<<"deactivate your account:"<<endl;
+    }
+
+    void updateBio(string newBio){
+        bio = newBio;
+    }
+};
 int main(){
-    student s1;
+    Student s1;
     s1.name = "shivank";
     s1.cgpa = 9.8;
     s1.calculateCgpa();
+
+    User u1;
+    u1.username = "rolex";
+    u1.password = "09u9uejfiweifwr2@#4h99h";
+    u1.bio = "Gamer";
+    u1.deactivate();
+    cout<<"old value of bio: "<<u1.bio<<endl;
+    u1.updateBio("Artist");
+    cout<<"new value of bio: "<<u1.bio<<endl;
     return 0;
 }
