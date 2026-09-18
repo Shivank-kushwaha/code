99ea0364f25730591deaa8d85ce56c80cb4de70a
shivank kushwaha <shivankkushwaha793@gmail.com>
2026-09-18 16:50:36 +0530

    copy constructor

---
+++ opps/copy.constructor.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+class Car{
+    string model;
+    string name;
+    string color;
+
+    public:
+
+    Car(string model, string name, string color){
+        this->model = model;
+        this->name = name; 
+        this->color = color;
+    }
+
+    void print(){
+        cout<<"model: "<<this->model<<endl;
+        cout<<"name: "<<this->name<<endl;
+        cout<<"color: "<<this->color<<endl;
+    }
+
+};
+
+int main(){
+    Car car1("xuv","tata","white");
+    Car car2(car1);
+
+    car2.print();
+
+    return 0;
+}
